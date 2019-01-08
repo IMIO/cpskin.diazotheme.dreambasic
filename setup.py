a172685bc064b2b36f8d0ebef4b2a88f4a40ae72
maud gennart <maud.gennart@imio.be>
2019-01-08 15:29:40 +0100

    Back to development: 0.3.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the cpskin.diazotheme.dreambasic package."""
 
-version = '0.3.7'
+version = '0.3.8.dev0'
 
 from setuptools import find_packages
 from setuptools import setup
