45c32be2718d1dc01bd473618ee822d4197e7f26
maud gennart <maud.gennart@imio.be>
2019-03-04 14:22:42 +0100

    Back to development: 0.3.9

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the cpskin.diazotheme.dreambasic package."""
 
-version = '0.3.8'
+version = '0.3.9.dev0'
 
 from setuptools import find_packages
 from setuptools import setup
