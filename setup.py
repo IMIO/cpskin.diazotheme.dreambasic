16968202c96a1d9b41e837aed31d0bbd70bdffc9
Laurent Lasudry <lasudry@gmail.com>
2022-01-20 15:28:43 +0100

    Back to development: 0.3.14

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the cpskin.diazotheme.dreambasic package."""
 
-version = '0.3.13'
+version = '0.3.14.dev0'
 
 from setuptools import find_packages
 from setuptools import setup
